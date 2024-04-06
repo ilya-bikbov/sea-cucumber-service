dafde0a90b573309f73010c7d48bd547e9fbb87d
Ilya Bikbov <ilya.bikbov@gmail.com>
2024-04-06 12:00:06 +0300

    fix port host

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const initApp = require("./app");
-
+const port = process.env.PORT || 3000;
 const server = http.createServer(initApp());
-server.listen(3000, () => {
-  console.log("Server listening on port 3000");
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Server listening on port ${port}`);
 });
