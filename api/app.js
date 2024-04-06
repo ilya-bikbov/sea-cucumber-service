9ba94f8e10c94f7196d79d617cbb995e62542418
Ilya Bikbov <ilya.bikbov@gmail.com>
2024-04-06 22:37:02 +0300

    fix vercel

--- api/app.js
+++ api/app.js
@@ -12,8 +12,8 @@ function initApp() {
   var app = express();
 
   // view engine setup
-  app.set("views", path.join(__dirname, "..", "views"));
-  app.set("view engine", "jade");
+  //app.set("views", path.join(__dirname, "..", "views"));
+  //app.set("view engine", "jade");
 
   app.use(cors());
   app.use(logger("dev"));
